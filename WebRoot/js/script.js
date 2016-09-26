0610b830106f08483aa65835a0cece642644f526
gulangzai <1871710810@qq.com>
2016-09-26 17:36:26 +0800

    添加博客模块

--- WebRoot/js/script.js
+++ WebRoot/js/script.js
@@ -206,14 +206,14 @@ function parallax() {
 ------------------------------------------------------*/
 
     // main directions
-      map = new GMaps({
+   /*   map = new GMaps({
         el: '#map', lat: 23.790223, lng: 90.414036, zoom: 13, zoomControl : true, 
         zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, panControl : false, scrollwheel: false
-      });
+      });*/
     // add address markers
-    map.addMarker({ lat: 23.790223, lng: 90.414036, title: 'BD InfoSys',
+   /* map.addMarker({ lat: 23.790223, lng: 90.414036, title: 'BD InfoSys',
       infoWindow: { content: '<p>Building # 2, Plot # 111, Road # 35, Gulshan - 2, Dhaka</p>' } });
-
+   */
 /*----------------------------------------------------*/
 /*	contact form
 ------------------------------------------------------*/
